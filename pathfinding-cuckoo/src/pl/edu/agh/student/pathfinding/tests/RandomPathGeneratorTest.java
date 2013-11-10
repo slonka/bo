977e6dbbf8de374e028f677980b7e60c169d42f5
Radziu <Radziu@Radziu-Laptok>
2013-11-10 16:18:16 +0100

    sorry

--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/tests/RandomPathGeneratorTest.java
+++ pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/tests/RandomPathGeneratorTest.java
@@ -21,34 +21,6 @@ public class RandomPathGeneratorTest{
 	}
 
 	@Test
-	public void getSolutionTest() {
-		Assert.assertTrue(generator.getSolution().getSteps().size() != 0);
-	}
-
-}
-package pl.edu.agh.student.pathfinding.tests;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import pl.edu.agh.student.pathfinding.RandomPathGenerator;
-import pl.edu.agh.student.pathfinding.map.IMap;
-import pl.edu.agh.student.pathfinding.map.BitmapMap;
-
-public class RandomPathGeneratorTest{
-
-	RandomPathGenerator generator;
-
-	@Before
-	public void setUp() throws Exception {
-		generator = new RandomPathGenerator(new BitmapMap(new File("maps/RandomPathGeneratorTest/RozjebutTest.png")));
-	}
-
-	@Test
 	public void getSolutionTest() throws IOException {
 		Assert.assertTrue(generator.getSolution().getSteps().size() != 0);
 		generator = new RandomPathGenerator(new BitmapMap(new File("maps/RandomPathGeneratorTest/PrzejsciaNiMa.png")));
