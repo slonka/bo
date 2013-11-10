db6dee135f4ee84c6db0ef2bc971fc94f1a9f3f2
Marek Mateja <marek@mateja.net.pl>
2013-11-10 15:42:17 +0100

    Generating solution image

--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/util/ISolutionImageBuilder.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.student.pathfinding.util;
-
-import java.awt.Image;
-
-public interface ISolutionImageBuilder {
-	Image generateImage();
-}
--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/util/SolutionImageBuilder.java
+++ pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/util/SolutionImageBuilder.java
@@ -1,21 +1,26 @@
 package pl.edu.agh.student.pathfinding.util;
 
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
 
 import pl.edu.agh.student.pathfinding.Solution;
+import pl.edu.agh.student.pathfinding.map.IMap;
 
-public class SolutionImageBuilder implements ISolutionImageBuilder {
+public class SolutionImageBuilder {
 	
-	private Solution solution;
+	private static int PATH_COLOR = 0x00FF0000;
 	
-	public SolutionImageBuilder(Solution solution) {
-		this.solution = solution;
-	}
-
-	@Override
-	public Image generateImage() {
-		// TODO Auto-generated method stub
-		return null;
+	public static Image generateImage(Solution solution) {
+		IMap map = solution.getMap();
+		
+		BufferedImage image = map.getMapImage();
+		
+		for(Point p : solution.getSteps()) {
+			image.setRGB(p.x, p.y, PATH_COLOR);
+		}
+		
+		return image;
 	}
 
 }
