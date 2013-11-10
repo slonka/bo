da52b86cf7bbd24ef2c41534e93d0aea90485313
slonka <krzysztof@slonka.net>
2013-11-10 14:08:00 +0100

    Updated Cuckoo Solver

--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/CuckooSolver.java
+++ pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/CuckooSolver.java
@@ -1,18 +1,29 @@
 package pl.edu.agh.student.pathfinding;
 
+import java.util.Random;
+
 import pl.edu.agh.student.pathfinding.map.IMap;
 
 public class CuckooSolver implements ISolver {
 	private IPathGenerator generator;
 	private int n;
-	private Solution nest[];
+	private Solution[] nest;
+	private int[] f; // quality/fitness
+	private int maxGeneration;
+	private IMap map;
+	private Random r = new Random();
+	private float pa; // Probability of Abandonment
+	private ISolutionModifier solutionModifier;
 	
-	
-	public CuckooSolver(int population, IPathGenerator generator) {		
+	public CuckooSolver(IMap map, IPathGenerator generator, ISolutionModifier solutionModifier, int population, int maxGeneration, float pa) {		
 		this.generator = generator;
 		this.n = population;
-		
-		nest = new Solution[population];
+		this.maxGeneration = maxGeneration;
+		this.map = map;
+		this.solutionModifier = solutionModifier;
+		this.pa = pa;
+		nest = new Solution[n];
+		f = new int[n];
 	}
 
 	@Override
@@ -21,8 +32,33 @@ public class CuckooSolver implements ISolver {
 		// Generate an initial population of  n  host nests
 		for(int i=0; i<n; i++) {
 			nest[i] = generator.getSolution();
+			f[i] = nest[i].f();
 		}
 		
+		for (int t=0; t<maxGeneration; t++) { // While (t<MaxGeneration) or (stop criterion)
+			// Get a cuckoo randomly (say, i) and replace its solution by performing random operations;
+			int i = r.nextInt(n);
+			Solution randomNest = nest[i];
+			randomNest = solutionModifier.modify(nest[i]);
+			
+			// Evaluate its quality/fitness
+			int fi = randomNest.f();
+			
+			//Choose a nest among n (say, j) randomly;
+			int j = r.nextInt(n);
+			int fj = f[j];
+			
+			if(fi > fj) {
+				nest[i] = randomNest;
+				f[i] = fi;
+			}
+			
+			
+			
+			// A fraction (pa) of the worse nests are abandoned and new ones are built;
+			
+			
+		}
 		
 		return null;
 	}
