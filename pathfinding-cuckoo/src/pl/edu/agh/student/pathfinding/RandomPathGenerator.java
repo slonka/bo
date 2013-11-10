e1433d7359240c265e65cc25d2614c05c0ffc704
Radziu <Radziu@Radziu-Laptok>
2013-11-10 15:43:22 +0100

    print

--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/RandomPathGenerator.java
+++ pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/RandomPathGenerator.java
@@ -38,22 +38,7 @@ public class RandomPathGenerator implements IPathGenerator{
 				newSolution.addStep(current);
 			}
 		}
-		List<Point> lista = newSolution.getSteps();
-		char[][] tablica = new char[10][10];
-		for(char[] wiersz : tablica){
-			for(char znak : wiersz){
-				znak = '*';
-			}
-		}
-		for(Point p : lista){
-			tablica[p.x][p.y]= 'x'; 
-		}
-		for(char[] wiersz : tablica){
-			for(char znak : wiersz){
-				System.out.print(znak);
-			}
-			System.out.println();
-		}
+		print(newSolution);
 		return newSolution;
 	}
 	
@@ -110,6 +95,25 @@ public class RandomPathGenerator implements IPathGenerator{
 				 (currentPoint.x < map.getWidth()-1 ? (visitationTable[currentPoint.x + 1][currentPoint.y] || !map.isAccessible(new Point(currentPoint.x + 1, currentPoint.y))) : true) &&
 				 (currentPoint.x < map.getWidth()-1 && currentPoint.y < map.getHeight()-1 ? (visitationTable[currentPoint.x + 1][currentPoint.y + 1] || !map.isAccessible(new Point(currentPoint.x + 1, currentPoint.y + 1))) : true)
 				);
+	}
+	
+	private void print(Solution newSolution){
+		List<Point> lista = newSolution.getSteps();
+		char[][] tablica = new char[10][10];
+		for(char[] wiersz : tablica){
+			for(char znak : wiersz){
+				znak = '*';
+			}
+		}
+		for(Point p : lista){
+			tablica[p.x][p.y]= 'x'; 
+		}
+		for(char[] wiersz : tablica){
+			for(char znak : wiersz){
+				System.out.print(znak);
+			}
+			System.out.println();
+		}
 	}
 
 }
