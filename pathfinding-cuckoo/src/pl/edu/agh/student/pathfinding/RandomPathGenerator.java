efe595964ac6c93fb5e4d49bf7b9d502b51540bb
Radziu <Radziu@Radziu-Laptok>
2013-11-13 13:43:25 +0100

    not necessary filling up of vistation table in constructor

--- pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/RandomPathGenerator.java
+++ pathfinding-cuckoo/src/pl/edu/agh/student/pathfinding/RandomPathGenerator.java
@@ -16,11 +16,6 @@ public class RandomPathGenerator implements IPathGenerator{
 	public RandomPathGenerator(IMap map){
 		this.map = map;
 		visitationTable = new boolean[map.getWidth()][map.getHeight()];
-		for(boolean[] verse : visitationTable){
-			for(boolean visitation : verse){
-				visitation = false;
-			}
-		}
 	}
 
 	@Override
